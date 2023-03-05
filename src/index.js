797acfcc6727f34f61337f5b1c58b4479159dd27
Alexander-Solonar <Alexander-Solonar_solo991@ukr.net>
2023-03-05 22:24:49 +0200

    add lib

--- src/index.js
+++ src/index.js
@@ -1,1 +1,28 @@
 import './css/styles.css';
+import Notiflix from 'notiflix';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const form = document.querySelector('#search-form');
+const gallery = document.querySelector('.gallery');
+const btnLoad = document.querySelector('.load-more');
+
+
+
+// <div class="photo-card">
+//   <img src="" alt="" loading="lazy" />
+//   <div class="info">
+//     <p class="info-item">
+//       <b>Likes</b>
+//     </p>
+//     <p class="info-item">
+//       <b>Views</b>
+//     </p>
+//     <p class="info-item">
+//       <b>Comments</b>
+//     </p>
+//     <p class="info-item">
+//       <b>Downloads</b>
+//     </p>
+//   </div>
+// </div>;
